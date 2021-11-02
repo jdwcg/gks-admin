9fdc3e26a1edffa86912eea2ab00f4e71ee20865
jdwcg <73451502+jdwcg@users.noreply.github.com>
2021-11-02 14:56:24 +0900

    11

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,16 @@ $(function(){
   var allPanels = $('.acc > ul').hide();
   // $('.acc ul:first-child').show();
 
-  $('.acc > h2').click(function() {
+  $('.acc > h2').click(function(e) {
+    e.preventDefault();
     allPanels.slideUp();
     $(this).next().slideDown('easing');
     return false;
   });
 
-  // $('.3depth').click(function(){
+  // $('.3depth').click(function(e){
+  //   e.preventDefault();
   //   $('.3depth ~ ul li').css({"background: red"})
-  // })
-
+  // });
 
 });
